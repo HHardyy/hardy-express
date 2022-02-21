e5c8c00a3bf2ded9e823958affa98cfced902b80
hardy <bingzhenmail@gmail.com>
2022-02-22 00:39:19 +0800

    application route layer

--- example/test.js
+++ example/test.js
@@ -1,10 +1,26 @@
-const express = require('../lib/express')
+// const express = require('../lib/express')\
+const express = require('express')
 const app = express()
 const host = 3000
 
 
-app.get('/', (req, res) => {
-  res.end('hello express')
+app.get('/', (req, res, next) => {
+  // res.end('hello express')
+  console.log(1);
+  next()
+}, (req, res, next) => {
+  console.log(11);
+  next()
+}, (req, res, next) => {
+  console.log(111);
+  next()
+})
+
+app.get('/', (req, res, next) => {
+  console.log(2);
+}, (req, res, next) => {
+  console.log(22);
+  res.end('router end')
 })
 
 app.listen(host, () => {
